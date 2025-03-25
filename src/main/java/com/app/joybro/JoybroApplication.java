1626084f3321fa9ac2331b32cafb712a743365d9
Daniel <macbookpro@Danielui-MacBookPro.local>
2025-03-25 21:41:25 +0900

    초기 커밋

---
+++ src/main/java/com/app/joybro/JoybroApplication.java
@@ -0,0 +1,13 @@
+package com.app.joybro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoybroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoybroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/joybro/JoybroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.joybro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoybroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
